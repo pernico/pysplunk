946d7c39b1d3284778a5af6f533f0fd9df8d3789
pernico <nicolas.perreault@gmail.com>
2017-02-28 15:25:07 -0500

    After changing name, bis

--- py_sc_converter.py
+++ py_sc_converter.py
@@ -2,26 +2,59 @@
 
 """ This tool converts a whitelist using ip addresses in parentheses to a search string to convert it to dns whitelisting """
 
-__author__ = "Nicolas Perreault"
-__version__ = "0.0.2"
+__author__ = "Nicolas P."
+__version__ = "0.0.3"
 __status__ = "Alpha"
 
 import argparse
 import urllib, urllib2
-from xml.dom import minidom
 import re
 
-parser = argparse.ArgumentParser(description='Splunk serverclass ip converter')
-parser.add_argument('whitelist', help='This contains the whitelist to convert')
-args = parser.parse_args()
+class ServerClass(object):
+    def __init__(self, fl):
+        self.apps = {}
+        i = 0
+        app_name = ''
+        for i in range(0, len(fl)):
+            if re.search('\[serverClass:[^:]+\]', fl[i]):
+                app_name = re.search('\[serverClass:([^:]+)\]', fl[i]).group(1)
+                self.apps[app_name] = [i]
+            elif re.search('whitelist\.\d+\s*=\s*[\d\.]+', fl[i]):
+                lst = re.search('whitelist\.\d+\s*=\s*([^\)]+\))', fl[i]).group(1)
+                self.apps[app_name].append(self.recompose_ip(lst))
 
-r = re.search("(whitelist\.\d+ = )?([\d\.]+)\(([0-9\.|]+)\)", args.whitelist)
-base = r.group(2)
-items = re.split(r'[|]',r.group(3))
+    def __repr__(self):
+        """ Print friendly object representation """
+        rep = '\nList of apps in serverclass.conf:\n\n'
+        for i in self.apps:
+            rep += 'app: %s,  index: %d\n' % (i, self.apps[i][0])
+            for a in range(1, len(self.apps[i])):
+                rep += '+ Whitelisted: ' + ', '.join(self.apps[i][a])
+                rep += '\n'
+        return rep
 
-recherche = """earliest=-1h@h index =_internal sourcetype="splunkd_access" method="POST" "/services/broker/phoneHome/" ("""
-for b in items[:-1]:
-  recherche += "clientip=%s%s OR " % (base, b)
-recherche += "clientip=%s%s) | stats sparkline count by src_clientname clientip src_dns src_hostname" % (base, b)
+    def recompose_ip(self, wl):
+        """ Function takes the line in old format and recompose all IPs """
+        ips = []
+        r = re.search('([\d\.]+)\(([\d\|]+)\)', wl)
+        base = r.group(1)
+        items = re.split(r'[|]',r.group(2))
+        for i in items:
+            ips.append("%s%s" % (base, i))
+        return ips
 
-print recherche
+
+def main():
+    parser = argparse.ArgumentParser(description='Splunk serverclass ip converter')
+    parser.add_argument('--path', default='serverclass.conf', help='path to serverclass.conf file')
+    args = parser.parse_args()
+
+    # Opening file in readonly and object initialisation
+    with open(args.path, 'r') as f:
+        sc = ServerClass(f.readlines())
+
+    # For sanity check, printing object informations
+    print sc
+
+if __name__ == '__main__':
+         main()
